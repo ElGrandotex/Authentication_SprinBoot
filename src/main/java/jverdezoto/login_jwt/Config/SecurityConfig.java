7e32e68c5bfa0e785ad443a3ab2f6001424f93bf
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-25 18:40:40 -0500

    Update SecurityConfig.java

--- src/main/java/jverdezoto/login_jwt/Config/SecurityConfig.java
+++ src/main/java/jverdezoto/login_jwt/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import jverdezoto.login_jwt.Jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,6 +29,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authRequest ->
                         authRequest
+                                .requestMatchers(HttpMethod.GET).permitAll()
+                                .requestMatchers(HttpMethod.OPTIONS).permitAll()
                                 //Se da acceso a la ruta auth e hijas
                                 .requestMatchers("/auth/**").permitAll()
                                 //Se niega acceso a otras rutas
