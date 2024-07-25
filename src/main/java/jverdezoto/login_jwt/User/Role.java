7876304d70d8900fdea4eb24a6e3ae33510054ff
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-25 01:34:57 -0500

    user configuration

---
+++ src/main/java/jverdezoto/login_jwt/User/Role.java
@@ -0,0 +1,6 @@
+package jverdezoto.login_jwt.User;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/jverdezoto/login_jwt/User/UserRepository.java
@@ -0,0 +1,12 @@
+package jverdezoto.login_jwt.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+//El JpaRepository se debe poner la clase (User) y
+//el tipo de identificador (Integer)
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
