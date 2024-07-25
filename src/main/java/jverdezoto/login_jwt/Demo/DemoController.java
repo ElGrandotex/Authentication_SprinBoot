d2ecf4f7070e39f5d26f8141d4787e24eda20248
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-25 18:43:05 -0500

    CORS-Angular

--- src/main/java/jverdezoto/login_jwt/Demo/DemoController.java
+++ src/main/java/jverdezoto/login_jwt/Demo/DemoController.java
@@ -1,6 +1,7 @@
 package jverdezoto.login_jwt.Demo;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class DemoController {
 
     @PostMapping(value = "demo")
