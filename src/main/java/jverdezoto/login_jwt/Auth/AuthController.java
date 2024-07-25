242999cbe55d838fbc672951f8c4589bfe3d2eaa
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-25 01:36:03 -0500

    Update AuthController.java

--- src/main/java/jverdezoto/login_jwt/Auth/AuthController.java
+++ src/main/java/jverdezoto/login_jwt/Auth/AuthController.java
@@ -1,7 +1,9 @@
 package jverdezoto.login_jwt.Auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping(value = "login")
-    public String login(){
-        return "Login";
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
     }
     @PostMapping(value = "register")
-    public String register(){
-        return "Register";
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.register(request));
     }
 }
