c8247f41f386d641dd1faee221ff7e1891989363
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-07-25 01:35:27 -0500

    configuration

--- src/main/java/jverdezoto/login_jwt/Config/SecurityConfig.java
+++ src/main/java/jverdezoto/login_jwt/Config/SecurityConfig.java
@@ -1,18 +1,25 @@
 package jverdezoto.login_jwt.Config;
 
+import jverdezoto.login_jwt.Jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
 
     //Restringir acceso
     @Bean
@@ -26,7 +33,11 @@ public class SecurityConfig {
                                 //Se niega acceso a otras rutas
                                 .anyRequest().authenticated()
                                 )
-                .formLogin(Customizer.withDefaults())
+                .sessionManagement(sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
